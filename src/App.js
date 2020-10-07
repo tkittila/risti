e0b21625ef5eee57928c71f3ff33e7f14645a322
Tuomo Kittila <tkittila75@gmail.com>
2020-10-07 12:48:40 +0300

    j

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-
-const Square =  (props) =>
+const Square = (props) =>
 {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={() =>  props.onClick()} >
       {props.value}
     </button>
   )
 }
 
+const Board = () => {
+  const [squares, setSquares] = useState( Array(9).fill(null))
+  
+  const handleClick = (i) => {
+    
+    
+    const newSquares = squares.slice();
+    newSquares[i] = 'X'
+    setSquares(newSquares)
+    console.log("clik", i, newSquares)
+  }
 
-function App() {
 
-  var squares = [ 1,2,3,4,5,6,7,8,9]
-
+  const rendSquare = (i) => {
+    return (<Square value={squares[i]} onClick= {() =>handleClick(i)}/>)
+  }
 
   return (
     <div>
       <div>
         <div className="board-row">
-        <Square value={squares[0]}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+          {rendSquare(0)}
+          {rendSquare(1)}
+          {rendSquare(2)}
+          </div>
+          <div className="board-row">
+          {rendSquare(3)}
+          {rendSquare(4)}
+          {rendSquare(5)}
+          </div>
+          <div className="board-row">
+          {rendSquare(6)}
+          {rendSquare(7)}
+          {rendSquare(8)}
         </div>
-        <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
-        </div>
-        <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
-        </div>
+      </div>
+    </div>
+  )
+
+  }
+
+function App() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
       </div>
     </div>
   );
